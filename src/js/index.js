1d8b6d1a62445c14f5d9614b9be13c17e85dacd6
ivan3123708 <ivan3123708@gmail.com>
2018-06-02 15:42:52 +0200

    Added details modal

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,21 @@ import numeral from 'numeral';
 import '../styles/index.sass';
 
 window.onload = () => {
+  const container = document.getElementById('container');
+  const shadow = document.getElementById('shadow');
+  const modal = document.getElementById('modal');
+  const logo = document.getElementById('logo');
+  const model = document.getElementById('model');
+  const from = document.getElementById('from');
+  const to = document.getElementById('to');
+  const modalClose = document.getElementById('modal-close');
+  modalClose.onclick = () => {
+    modal.classList.remove('modal-open');
+    modal.classList.add('modal-closed');
+    shadow.classList.remove('shadow-on');
+    shadow.classList.add('shadow-off');
+  };
+
   function handlePosition(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
@@ -13,13 +28,28 @@ window.onload = () => {
       url,
       dataType: 'jsonp',
       success: (data) => {
-        const container = document.getElementById('container');
-        const list = data.acList.sort((a, b) => a.Alt < b.Alt);
+        const list = data.acList.sort((a, b) => {
+          if (a.Alt < b.Alt) return 1;
+          else if (a.Alt > b.Alt) return -1;
+
+          return 0;
+        });
 
         list.forEach((aircraft) => {
           const listingItem = document.createElement('div');
           listingItem.setAttribute('id', 'listing-item');
           listingItem.setAttribute('title', 'Click to show more');
+          listingItem.onclick = () => {
+            modal.classList.remove('modal-closed');
+            modal.classList.add('modal-open');
+            shadow.classList.remove('shadow-off');
+            shadow.classList.add('shadow-on');
+
+            model.innerHTML = aircraft.Mdl || 'N/A';
+            logo.setAttribute('src', `https://logo.clearbit.com/${aircraft.Op.toLowerCase().replace(/\s/g, '')}.com`);
+            from.innerHTML = `<b>DEPARTURE</b><br><br>${aircraft.From || 'N/A'}`;
+            to.innerHTML = `<b>ARRIVAL</b><br><br>${aircraft.To || 'N/A'}`;
+          };
 
           const icon = document.createElement('img');
           icon.setAttribute('id', 'icon');
@@ -27,11 +57,11 @@ window.onload = () => {
 
           const altitude = document.createElement('p');
           altitude.setAttribute('id', 'altitude');
-          altitude.innerHTML = `Altitude: <b>${numeral(aircraft.Alt).format('0,0')} ft</b>`;
+          altitude.innerHTML = `Altitude: <b>${numeral(aircraft.Alt).format('0,0') || 'N/A'} ft</b>`;
 
           const flightCode = document.createElement('p');
           flightCode.setAttribute('id', 'flight-code');
-          flightCode.innerHTML = `Flight Code: <b>${aircraft.Call}</b>`;
+          flightCode.innerHTML = `Flight Code: <b>${aircraft.Call || 'N/A'}</b>`;
 
           listingItem.appendChild(icon);
           listingItem.appendChild(altitude);
